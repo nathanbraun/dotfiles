cc4d3e26e87afbfabf0138659a1d849c58b8351a
Nathan Braun <nathanericbraun@gmail.com>
2024-03-12 09:25:10 -0500

    fix: remove deprecated context_commentstring configuration

--- private_dot_config/nvim/lua/user/treesitter.lua
+++ private_dot_config/nvim/lua/user/treesitter.lua
@@ -1,23 +1,29 @@
+vim.g.skip_ts_context_commentstring_module = true
+
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
   return
 end
 
+
+
 configs.setup {
-  ensure_installed = {"python"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = {}, -- List of parsers to ignore installing
+  ensure_installed = {"python"}, -- one of "all", "maintained", or a list of languages
+  sync_install = false,
+  ignore_install = {},
   autopairs = {
     enable = true,
   },
   highlight = {
-    enable = true, -- false will disable the whole extension
+    enable = true,
     disable = {}, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true, disable = { "yaml"} },
-  context_commentstring = {
+  indent = {
     enable = true,
-    enable_autocmd = false,
+    disable = { "yaml" }
   },
+  -- Removed the deprecated context_commentstring configuration from here
 }
+
+require('ts_context_commentstring').setup {}
