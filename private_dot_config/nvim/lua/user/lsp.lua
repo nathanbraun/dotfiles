9714e156d2752cd2314a17221b0c53b8d970b8fc
Nathan Braun <nathanericbraun@gmail.com>
2024-06-30 22:50:32 -0500

    feat: add language servers

--- private_dot_config/nvim/lua/user/lsp.lua
+++ private_dot_config/nvim/lua/user/lsp.lua
@@ -20,7 +20,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'elmls', 'pyright'},
+  ensure_installed = {'elmls', 'pyright', 'tsserver', 'hls'},
   handlers = {
     lsp_zero.default_setup,
   }
